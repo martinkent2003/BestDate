a524fed870309c12e135278964c87c67a00eb691
Martin Kent <martinkent@ufl.edu>
2023-05-11 12:34:43 -0400

    reactive forms and user registration

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,6 +6,12 @@ namespace API.DTOs
     {
         [Required]
         public string Username {get; set;}
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        [Required] public DateOnly? DateOfBirth { get; set; } //optional to make required work (ik it sounds weird) cannot be null unless we make it optional, which is necesarry for a required field
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
+
         [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password {get; set;}
